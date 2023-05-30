483048a0fcfac856f757563b2fc593f246fb93da
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2023-05-30 21:17:08 +0200

    issue 2 tests ok

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,68 @@
 import pytest
 
+import server
 from server import app
-
-
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
 
 
 """@pytest.fixture
 def client():
+    with app.test_client() as client:
+        yield client"""
+
+
+@pytest.fixture
+def client():
     app.config['TESTING'] = True
     client = app.test_client()
-    return client"""
+    return client
+
+
+@pytest.fixture
+def mock_clubs(mocker):
+    clubs = [
+        {
+            "name": "Test Club 1",
+            "email": "club1@test.com",
+            "points": "40"
+        },
+        {
+            "name": "Test Club 2",
+            "email": "club2@test.com",
+            "points": "25"
+        },
+        {
+            "name": "Test Club 3",
+            "email": "club3@test.com",
+            "points": "2"
+        },
+    ]
+    mocked = mocker.patch.object(server, 'clubs', clubs)
+    yield mocked
+
+
+@pytest.fixture
+def mock_competitions(mocker):
+    competitions = [
+        {
+            "name": "Test Competition 1",
+            "date": "2023-06-07 10:00:00",
+            "numberOfPlaces": "25"
+        },
+        {
+            "name": "Test Competition 2",
+            "date": "2023-03-27 10:00:00",
+            "numberOfPlaces": "13"
+        },
+        {
+            "name": "Test Competition 3",
+            "date": "2022-06-09 10:00:00",
+            "numberOfPlaces": "5"
+        },
+        {
+            "name": "Test Competition 4",
+            "date": "2020-06-09 10:00:00",
+            "numberOfPlaces": "25"
+        }
+    ]
+    mocked = mocker.patch.object(server, 'competitions', competitions)
+    yield mocked
--- tests/test_*.py
+++ tests/test_*.py
@@ -1,29 +1,27 @@
 import json
 
-from server import clubs, competitions, purchasePlaces
 
-
-def test_book_should_status_code_ok(client):
-    club = clubs[0]['name']
-    competition = competitions[0]['name']
+def test_book_should_status_code_ok(client, mock_clubs, mock_competitions):
+    club = mock_clubs[0]['name']
+    competition = mock_competitions[0]['name']
     response = client.get(f'/book/{competition}/{club}')
     assert response.status_code == 200
 
 
-def test_purchasePlaces_should_booked_places_less_than_allowed_points(client):
-    club = clubs[0]['name']
-    competition = competitions[0]['name']
+def test_purchasePlaces_should_booked_places_less_than_allowed_points(client, mock_clubs, mock_competitions):
+    club = mock_clubs[0]['name']
+    competition = mock_competitions[0]['name']
     places = 10
     response = client.post('/purchasePlaces', data={'club': club, 'competition': competition, 'places': places})
     assert response.status_code == 200
-    assert int(clubs[0]['points']) >= 0
+    assert int(mock_clubs[0]['points']) >= 0
     assert "Great-booking complete!" in response.data.decode()
 
 
-def test_purchasePlaces_not_enough_points(client):
-    club = clubs[0]['name']
-    competition = competitions[0]['name']
-    places = 14
+def test_purchasePlaces_not_enough_points(client, mock_clubs, mock_competitions):
+    club = mock_clubs[0]['name']
+    competition = mock_competitions[0]['name']
+    places = 41
     response = client.post('/purchasePlaces', data={'club': club, 'competition': competition, 'places': places})
     assert response.status_code == 200
     assert "You do not have enough points" in response.data.decode()
